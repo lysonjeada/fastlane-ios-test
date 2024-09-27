4c085a9318581c7e19e69041deda12832893691f
lys <lyscalefi@gmail.com>
2024-09-27 14:37:15 -0300

    add ios project

---
+++ fastlane-ios-test/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  fastlane-ios-test
+//
+//  Created by Amaryllis Rosemaria Baldrez Calefi on 27/09/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ fastlane-ios-test/fastlane_ios_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  fastlane_ios_testApp.swift
+//  fastlane-ios-test
+//
+//  Created by Amaryllis Rosemaria Baldrez Calefi on 27/09/24.
+//
+
+import SwiftUI
+
+@main
+struct fastlane_ios_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
